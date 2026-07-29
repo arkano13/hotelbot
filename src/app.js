38c41e1c45267b1948ffbb9b0a91fd8ad555c52d
Gelsyn <gelsynfigu@gmail.com>
2026-07-29 01:11:22 -0600

    nose

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ import { apiLimiter } from "./lib/rateLimit.js";
 
 const app = express();
 
-app.set("trust proxy", 1);
+
 
 const ORIGENES_PERMITIDOS = (process.env.CORS_ORIGINS || "")
   .split(",")
