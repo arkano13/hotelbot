084fe60d1354599f3faa01223411c5a289dc43c1
Gelsyn <gelsynfigu@gmail.com>
2026-07-28 23:35:57 -0600

    rate limit

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ import { apiLimiter } from "./lib/rateLimit.js";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 const ORIGENES_PERMITIDOS = (process.env.CORS_ORIGINS || "")
   .split(",")
   .map((origen) => origen.trim())
